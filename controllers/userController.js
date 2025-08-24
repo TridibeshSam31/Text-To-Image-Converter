752ab4749f313513a87043eb852c32fcdc27fcdc
TridibeshSam31 <tridisam651@gmail.com>
2025-08-25 00:48:52 +0530

    update credit balance

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import  jwt  from "jsonwebtoken";
 import razorpay from "razorpay" 
+import transactionModel from "../models/transactionModel.js";
 //yahan par hum user ko login aur register krne ke liye conytroller likhenge jaise humne demoproject mai kiya tha
 
 const registerUser = async (req, res) => {
@@ -76,10 +77,74 @@ const razorpayPayment = async (req, res) => {
         return res.json({sucess:false , message:'Missing Details'})
      }
      let credits,plan,amount,date
+     date = Date.now()
+     const transactionData = {
+         userId,
+         plan,
+         amount,
+         credits,
+         date
+     }
+         const newTransaction = await transactionModel.create(transactionData) //kuch nhi bas database mai save krwa diya ab
+
+         //order ko create krne ke liye kya krenge 
+
+         await razorpayInstance.orders.create({
+             amount: newTransaction.amount,
+             currency: "INR",
+             receipt: newTransaction._id.toString()
+         });
+
     } catch (error) {
         console.log(error)
         res.json({sucess:false,message:error.message})
     }
 }*/
+
+
+/*
+ab payment jo krenge usko verify kaise krenge usko very krne ke liye bhi humme function likhna hoga
+lekin hoga woh function asyncronous because hume database se transaction ki details lene hain aur woh bhi asyncronous hoga
+toh hum ek async function likhenge jo payment verification karega
+
+const verifyRazorpay = async ()=>{
+    try {
+        const {razorpay_order_id} = req.body
+
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if(orderInfo.status === 'paid){
+        const transacctionData = await transactionModel.findById(orderInfo.receipt)
+        if(!transacctionData.payment){
+        return res.json({sucess:false,message:'Payment not verified'})
+        }
+
+        const userData = await userModel.findById(transactionData.userId)
+        ab jo paise diye hai usse credit balance bhi toh add krenge so
+
+        const creditBalance = userData.creditBalance + transactionData.amount
+        await userModel.findByIdAndUpdate(transactionData._id,{creditBalance})
+        await transactionModel.findByIdAndUpdate(transactionData._id,{payment:true})
+        return res.json({sucess:true,message:'Payment verified and credits added'})
+     }else{
+         return res.json({sucess:false,message:'Payment not verified'})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({sucess:false,message:error.message})
+    }
+}
+
+
+
+
+
+*/
+
+
+
+
+
+
 
 export {registerUser,loginUser,userCredits} 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import { registerUser,loginUser, userCredits } from '../controllers/userController.js'
 import userAuth from '../middleware/auth.js'
+//import { razorpayPayment } from '../controllers/paymentController.js'
+//import { verifyRazorpay } from '../controllers/paymentController.js'
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 //userRouter.post('/credits',userCredits) isko use krne ke liye obv humko middleware mai hi jana pdega 
 userRouter.post('/credits',userAuth,userCredits)
+
+//userRouter.post('/pay-razorpay',userAuth,razorpayPayment)
+//userRouter.post('/verify-razorpay'verifyRazorpay)//justified hai ab kyu le kr aaunga middleare ko isme
 
 export default userRouter
